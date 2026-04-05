1ed77fad7cfa22c40ae06426cea9d13eb7f76144
Felix Schöller <felix.schoeller47@gmail.com>
2026-04-05 13:03:43 +0200

    chore: move excercise 1 implementation into repo

--- server/files/index.js
+++ server/files/index.js
@@ -1,24 +1,102 @@
-window.onload = function () {
-  const xhr = new XMLHttpRequest();
-  xhr.onload = function () {
-    const bodyElement = document.querySelector("body");
-    if (xhr.status == 200) {
-      const movies = JSON.parse(xhr.responseText);
-      for (const movie of movies) {
-        /* Task 1.3. Add your code from exercise 1 here 
-           and include a non-functional 'Edit' button
-           to pass this test */
-      }
+function displayMovie(movie) {
+    //Container
+    const container = document.createElement("article")
 
-    } else {
-      bodyElement.append(
-        "Daten konnten nicht geladen werden, Status " +
-          xhr.status +
-          " - " +
-          xhr.statusText
-      );
+    //Poster
+    const poster = document.createElement("img")
+    poster.src = movie.Poster
+    container.appendChild(poster)
+
+    //Child-container
+    const childContainer = document.createElement("div")
+
+        //Title-container
+        const titleContainer = document.createElement("div")
+        titleContainer.classList.add("article-header");
+
+            //Title
+            const title = document.createElement("h2")
+            title.textContent = movie.Title
+            titleContainer.appendChild(title)
+
+            // Info (Released, Runtime, Rating)
+            const info = document.createElement("p")
+            info.textContent = `Released: ${movie.Released} | Runtime: ${movie.Runtime} min | IMDb: ${movie.imdbRating} | Metascore: ${movie.Metascore}`
+            titleContainer.appendChild(info)
+
+        childContainer.appendChild(titleContainer)
+
+        // Genres
+        const genres = generateTagsElement(movie.Genres)
+        childContainer.appendChild(genres)
+
+        // Plot
+        const plot = document.createElement("p")
+        plot.textContent = movie.Plot
+        childContainer.appendChild(plot)
+
+        //Directors
+        const directors = generateListElement("Directors", movie.Directors)
+        childContainer.appendChild(directors)
+
+        //Writers
+        const writers = generateListElement("Writers", movie.Writers)
+        childContainer.appendChild(writers)
+        
+        //Actors
+        const actors = generateListElement("Actors", movie.Actors)
+        childContainer.appendChild(actors)
+
+    container.appendChild(childContainer)
+
+    return container
+}
+
+function generateListElement(listTitle, list){
+    const element = document.createElement("p")
+
+    const title = document.createElement("h3");
+    title.textContent = listTitle;
+    element.appendChild(title)
+
+    const unsortedList = document.createElement("ul")
+    for(const item of list){
+        const listItem = document.createElement("li")
+        listItem.textContent = item
+        unsortedList.appendChild(listItem)
     }
-  };
-  xhr.open("GET", "/movies");
-  xhr.send();
-};
+
+    element.appendChild(unsortedList)
+
+    return element
+}
+
+function generateTagsElement(list){
+    const element = document.createElement("div")
+
+    for(const item of list){
+        const span = document.createElement("span")
+        span.textContent = item
+        span.classList.add("genre-tag");
+        element.appendChild(span)
+    }
+
+    return element
+}
+
+window.onload = function () {
+    const xhr = new XMLHttpRequest()
+    xhr.onload = function () {
+        const bodyElement = document.querySelector("body")
+        if (xhr.status == 200) {
+            const movies = JSON.parse(xhr.responseText)
+            for(const movie of movies){
+                bodyElement.append(displayMovie(movie))
+            }
+        } else {
+            bodyElement.append("Daten konnten nicht geladen werden, Status " + xhr.status + " - " + xhr.statusText)
+        }
+    }
+    xhr.open("GET", "/movies")
+    xhr.send()
+}
--- server/server.js
+++ server/server.js
@@ -12,10 +12,11 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'files')));
 
 // Configure a 'get' endpoint for all movies..
-app.get('/movies', function (req, res) {
-  /* Task 1.2. Remove the line below and eturn the movies from 
+app.get('/movies', async function (req, res) {
+    /* Task 1.2. Remove the line below and eturn the movies from 
      the model as an array */
-  res.sendStatus(404)
+  const movies = await loadJSONs()
+  res.json(movies)
 })
 
 // Configure a 'get' endpoint for a specific movie
@@ -34,3 +35,17 @@ app.listen(3000)
 
 console.log("Server now listening on http://localhost:3000/")
 
+async function loadJSONs() {
+  const fs = require("fs");
+
+  const folder = path.join(__dirname, "files/json");;
+
+  const array = fs.readdirSync(folder)
+    .filter(file => file.endsWith(".json"))
+    .map(file => {
+      const content = fs.readFileSync(path.join(folder, file));
+      return JSON.parse(content);
+    });
+
+  return array; // Array mit allen JSON-Inhalten
+}
